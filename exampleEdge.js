dc5aa3f5ab5fd09eae6bfd0182872b3725e905a6
rahulraj <rahulraj@qburst.com>
2019-09-02 12:15:35 +0530

    30/08/2019 changes

--- exampleEdge.js
+++ exampleEdge.js
@@ -1,4 +1,4 @@
-const pptrEdge = require('puppeteer-edge');
+/*const pptrEdge = require('puppeteer-edge');
 
 (async () => {
   const browser = await pptrEdge.launch({headless: false,
@@ -7,4 +7,24 @@ const pptrEdge = require('puppeteer-edge');
   const page = await browser.newPage();
   await page.goto("https://pptr.dev");
   await page.screenshot({path: 'exampleEdge.png'});
-})()
+})()*/
+var puppeteer = require('puppeteer');
+(async () => {
+  /*const browserOptions = new LaunchOptions
+{
+    Headless = false,
+    ExecutablePath = "/Applications/Microsoft\ Edge\ Beta.app"
+};*/
+
+const browser = await puppeteer.launch({
+  Headless : false,
+  ExecutablePath : "/Applications/Microsoft\ Edge\ Beta.app",
+  slowMo:200
+});
+const page = await browser.newPage();
+console.log('in process');
+await page.goto('https://www.google.com');
+  await page.screenshot({path: 'exampleEdge.png'});
+  
+//await page.SetContentAsync('<div>Testing</div>');
+})();
